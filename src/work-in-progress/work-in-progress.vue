e51d8ebe9a755cf4bb9c6d36acb877218ea56eca
Starelie <timotheelaberge@gmail.com>
2025-10-31 18:09:04 -0400

    Changed the wip page by adding a link to the email for questions and a small sentence explaining that we are currently working on the page

--- src/work-in-progress/work-in-progress.vue
+++ src/work-in-progress/work-in-progress.vue
@@ -7,7 +7,7 @@ import BaseFooter from '../../src/components/BaseFooter.vue'
   <BaseHeader/>
   <main>
     <h1>Work in Progress</h1>
-    <h3>Check back soon for updates!</h3>
+    <h3>Our students are currently working on getting this page up and running. If you have any questions, you can contact us at <a href="mailto:stanrobotix@gmail.com">stanrobotix@gmail.com</a>. Check back soon for updates!</h3>
   </main>
   <BaseFooter/>
 </template>
