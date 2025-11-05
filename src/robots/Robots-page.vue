64ec185711f5a6daa36fc0ff2388ff25fadf541f
Starelie <timotheelaberge@gmail.com>
2025-11-05 18:10:24 -0500

    Début de la rédaction des descriptions des robotx

--- src/robots/Robots-page.vue
+++ src/robots/Robots-page.vue
@@ -5,7 +5,7 @@ import RobotCardComponent from '../../src/components/RobotCardComponent.vue';
 </script>
 <template>
 
-  <BaseHeader/>
+  <BaseHeader />
 
   <main>
     <h1>Robots</h1>
@@ -13,9 +13,8 @@ import RobotCardComponent from '../../src/components/RobotCardComponent.vue';
     <nav>
       <a href="#reefscape_2025">
         <h2>2025</h2>
-        <img
-          src="../components/images/Competition_logos/2025_REEFSCAPE/2025_logo_text.png"
-          class="logo" min-width="336px">
+        <img src="../components/images/Competition_logos/2025_REEFSCAPE/2025_logo_text.png" class="logo"
+          min-width="336px">
       </a>
       <a href="#crescendo_2024">
         <h2>2024</h2>
@@ -43,55 +42,42 @@ import RobotCardComponent from '../../src/components/RobotCardComponent.vue';
       <i class="fa-solid fa-chevron-up arrow"></i>
       <RobotCardComponent side="left" year="reefscape_2025">
         <template v-slot:logo>
-          <img src="../components/images/Competition_logos/2025_REEFSCAPE/2025_logo_patch.png" min-width="116px" width="100%">
+          <img src="../components/images/Competition_logos/2025_REEFSCAPE/2025_logo_patch.png" min-width="116px"
+            width="100%">
         </template>
         <template v-slot:img>
           <img src="../components/images/Robot_images/2025_robot.jpg" width="100%">
         </template>
         <template v-slot:desc>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Sed leo nascetur integer facilisis accumsan aliquet
-          elementum phasellus vitae. Tempus dis mauris integer nostra hac. Metus porta lacus ultricies fermentum
-          mollis
-          curabitur pharetra risus. Nam nullam class imperdiet iaculis felis taciti fringilla porta. Congue taciti leo
-          bibendum feugiat tristique. Nam dictumst id ligula at scelerisque ex euismod. Imperdiet nisl natoque varius
-          mauris quisque. Consequat lacinia nascetur tellus dignissim nibh, dolor neque eleifend.
-          <br>
-          Et dis nostra maximus suscipit cursus viverra quisque. Cursus malesuada elementum euismod consequat
-          condimentum pellentesque vulputate. Curabitur leo praesent amet maecenas, rhoncus facilisi adipiscing. Magna
-          magnis donec egestas dui erat vitae fames dui. Faucibus integer enim faucibus; sem et nisl! Augue felis
-          nostra
-          inceptos; curae ultrices accumsan. Sed finibus faucibus vitae vulputate accumsan ad. Blandit neque ipsum
-          dictumst sociosqu pretium non.
+          Our robot for Reefscape had three mecanisms: the first one was able to pickup coral on the ground and on for
+          the feeding station, the second one was able to grab algea from the ground and put it into the processor, the
+          third one was able to reach up and remove the algea from the reef and drop it on the ground to be later picked
+          up.
         </template>
         <template v-slot:date>Reefscape<sup>SM</sup> - 2025</template>
       </RobotCardComponent>
       <RobotCardComponent side="right" year="crescendo_2024">
         <template v-slot:logo>
-          <img src="../components/images/Competition_logos/2024_CRESCENDO/Horizontal/Full Color/FIS_CRESCENDO_Logo_Horizontal_RGB.png" min-width="116px" width="100%">
+          <img
+            src="../components/images/Competition_logos/2024_CRESCENDO/Horizontal/Full Color/FIS_CRESCENDO_Logo_Horizontal_RGB.png"
+            min-width="116px" width="100%">
         </template>
         <template v-slot:img>
           <img src="../components/images/Robot_images/2024_robot.jpg" width="100%">
         </template>
         <template v-slot:desc>
-          Lorem ipsum odor amet, consectetuer adipiscing elit. Sed leo nascetur integer facilisis accumsan aliquet
-          elementum phasellus vitae. Tempus dis mauris integer nostra hac. Metus porta lacus ultricies fermentum
-          mollis
-          curabitur pharetra risus. Nam nullam class imperdiet iaculis felis taciti fringilla porta. Congue taciti leo
-          bibendum feugiat tristique. Nam dictumst id ligula at scelerisque ex euismod. Imperdiet nisl natoque varius
-          mauris quisque. Consequat lacinia nascetur tellus dignissim nibh, dolor neque eleifend.
-          <br>
-          Et dis nostra maximus suscipit cursus viverra quisque. Cursus malesuada elementum euismod consequat
-          condimentum pellentesque vulputate. Curabitur leo praesent amet maecenas, rhoncus facilisi adipiscing. Magna
-          magnis donec egestas dui erat vitae fames dui. Faucibus integer enim faucibus; sem et nisl! Augue felis
-          nostra
-          inceptos; curae ultrices accumsan. Sed finibus faucibus vitae vulputate accumsan ad. Blandit neque ipsum
-          dictumst sociosqu pretium non.
+          Our robot for Crescendo had an arm with an intake able to pickup from the ground and output into the Amp. This
+          arm was installed on an elevator which was also used as our climbing mechanism with the help of hooks mounted
+          on the elevator. We were able to score in the trap during endgame but multiple problems led to this never
+          happening during a match.
         </template>
         <template v-slot:date>Crescendo<sup>SM</sup> - 2024</template>
       </RobotCardComponent>
       <RobotCardComponent side="left" year="charged_up_2023">
         <template v-slot:logo>
-          <img src="../components/images/Competition_logos/2023_CHARGED UP/Vertical/Full Color/CHARGED_UP_Logo_Vert_RGB_FullColor.png" min-width="116px" width="100%">
+          <img
+            src="../components/images/Competition_logos/2023_CHARGED UP/Vertical/Full Color/CHARGED_UP_Logo_Vert_RGB_FullColor.png"
+            min-width="116px" width="100%">
         </template>
         <template v-slot:img>
           <img src="../components/images/Robot_images/2023_robot.jpg" width="100%">
@@ -115,7 +101,9 @@ import RobotCardComponent from '../../src/components/RobotCardComponent.vue';
       </RobotCardComponent>
       <RobotCardComponent side="right" year="rapid_react_2022">
         <template v-slot:logo>
-          <img src="../components/images/Competition_logos/2022_RAPID REACT/Vertical/Full Color/RAPID_REACT_Logo_Vertical_RGB_FullColor.png" min-width="224px" width="100%">
+          <img
+            src="../components/images/Competition_logos/2022_RAPID REACT/Vertical/Full Color/RAPID_REACT_Logo_Vertical_RGB_FullColor.png"
+            min-width="224px" width="100%">
         </template>
         <template v-slot:img>
           <img src="../components/images/Robot_images/2022_robot.jpg" width="100%">
@@ -169,5 +157,4 @@ nav {
     transform: translateX(-50%) translateY(-10px);
   }
 }
-
 </style>
