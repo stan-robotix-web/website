e14c4858f6212f320baf51516b7a8b78bfd18a10
Aezurly <alice.vergeau@gmail.com>
2025-12-10 18:36:35 -0500

    fix: instagram feed behold

--- src/components/HomeInsta.vue
+++ src/components/HomeInsta.vue
@@ -1,17 +1,20 @@
 <script setup>
 (() => {
-    const d=document,s=d.createElement("script");s.type="module";
-    s.src="https://w.behold.so/widget.js";d.head.append(s);
-  })();
+  const d = document,
+    s = d.createElement("script");
+  s.type = "module";
+  s.src = "https://w.behold.so/widget.js";
+  d.head.append(s);
+})();
 </script>
 
 <template>
   <section>
-  <main>
-    <h2>Instagram <i class="fa-brands fa-instagram"></i></h2>
-    <behold-widget feed-id="YU2lTNeHVbqDa4dSU3VF"></behold-widget>
-  </main>
-</section>
+    <main>
+      <h2>Instagram <i class="fa-brands fa-instagram"></i></h2>
+      <behold-widget feed-id="OXReOhMVH3clzilNHD0S"></behold-widget>
+    </main>
+  </section>
 </template>
 
 <style scoped lang="scss">
@@ -19,11 +22,11 @@
 
 main {
   padding: 20px;
-  width:60vw;
-  margin:auto;
+  width: 60vw;
+  margin: auto;
 }
 
-main>h2 {
+main > h2 {
   font-weight: 600;
   font-size: 300%;
   line-height: 140%;
@@ -34,14 +37,13 @@ main>h2 {
   border-bottom: 4px solid $accent;
 
   i {
-    color:$accent;
+    color: $accent;
   }
 }
 
-@media screen and (max-width: 900px) { 
+@media screen and (max-width: 900px) {
   main {
-    width:90vw;
+    width: 90vw;
   }
 }
-
 </style>
